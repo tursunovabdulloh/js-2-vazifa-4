8c1f0600e6d99db69c4b75e0359015bb9f4217a2
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-05 03:56:12 +0300

    1

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 let section = document.querySelector("#section");
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
 function randomColor() {
   let rang1 = Math.floor(Math.random() * 255);
   let rang2 = Math.floor(Math.random() * 255);
   let rang3 = Math.floor(Math.random() * 255);
   return `rgb(${rang1}, ${rang2}, ${rang3})`;
 }
-section.style.backgroundColor = randomColor();
+function gradus() {
+  let gradus1 = Math.floor(Math.random() * 180);
+  return gradus1;
+}
+button1.style.backgroundColor = randomColor();
+button1.style.backgroundImage = `linear-gradient( 45deg, ${randomColor()}, ${randomColor()}, ${randomColor()})`;
+
+function randomColor2() {
+  let rang1 = Math.floor(Math.random() * 255);
+  let rang2 = Math.floor(Math.random() * 255);
+  let rang3 = Math.floor(Math.random() * 255);
+  return `rgb(${rang1}, ${rang2}, ${rang3})`;
+}
+function gradus2() {
+  let gradus1 = Math.floor(Math.random() * 180);
+  return gradus1;
+}
+button2.style.backgroundColor = randomColor2();
+button2.style.backgroundImage = `linear-gradient( 85deg, ${randomColor()}, ${randomColor()}, ${randomColor()})`;
