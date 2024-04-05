8604fb9170846d5aa1885bf7c74f69c38c0d04fc
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-05 13:50:42 +0300

    1

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-let section = document.querySelector("#section");
-let button1 = document.querySelector("#button1");
-let button2 = document.querySelector("#button2");
+let button = document.querySelector("#button");
 function randomColor() {
   let rang1 = Math.floor(Math.random() * 255);
   let rang2 = Math.floor(Math.random() * 255);
@@ -11,18 +9,62 @@ function gradus() {
   let gradus1 = Math.floor(Math.random() * 180);
   return `${gradus1}deg`;
 }
-button1.style.backgroundColor = randomColor();
-button1.style.backgroundImage = `linear-gradient( ${gradus()}, ${randomColor()}, ${randomColor()}, ${randomColor()})`;
+container.style.backgroundColor = randomColor();
+container.style.backgroundImage = `linear-gradient( ${gradus()}, ${randomColor()}, ${randomColor()}, ${randomColor()})`;
 
-function randomColor2() {
-  let rang1 = Math.floor(Math.random() * 255);
-  let rang2 = Math.floor(Math.random() * 255);
-  let rang3 = Math.floor(Math.random() * 255);
-  return `rgb(${rang1}, ${rang2}, ${rang3})`;
-}
-function gradus2() {
-  let gradus1 = Math.floor(Math.random() * 180);
-  return gradus1;
-}
-button2.style.backgroundColor = randomColor2();
-button2.style.backgroundImage = `linear-gradient( ${gradus()}, ${randomColor()}, ${randomColor()}, ${randomColor()})`;
+button.addEventListener("click", () => {
+  container.style.backgroundColor = randomColor();
+  container.style.backgroundImage = `linear-gradient( ${gradus()}, ${randomColor()}, ${randomColor()}, ${randomColor()})`;
+});
+
+// akhror web
+// const body = document.querySelector("body");
+// const container = document.querySelector(".container");
+// const colorText = document.querySelector(".color-text");
+// const values = [
+//   "0",
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+// ];
+
+// // random color function
+// function getGradient() {
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     const randomNumber = Math.trunc(Math.random() * values.length);
+//     color += values[randomNumber];
+//   }
+
+//   return color;
+// }
+
+// // set color to body
+// function setGradient() {
+//   const color1 = getGradient();
+//   const color2 = getGradient();
+//   const randomDeg = Math.trunc(Math.random() * 360);
+//   const bgColor = `linear-gradient(
+//     ${randomDeg}deg,
+//     ${color1},
+//     ${color2}
+//   )`;
+//   body.style.background = bgColor;
+//   colorText.textContent = bgColor;
+// }
+
+// setGradient();
+
+// container.addEventListener("click", setGradient);
